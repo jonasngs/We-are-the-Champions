b242303fa0c1b6b38bf381e356919cd90a673ae8
jonasngs <jonasngs30@gmail.com>
2022-08-16 22:30:42 +0800

    Add registration component

---
+++ client/src/components/NewRegistration.js
@@ -0,0 +1,52 @@
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+
+const NewRegistration = () => {
+  const [teamInfo, setTeamInfo] = useState('');
+  const [errorMessage, setError] = useState('');
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/registration', {
+        teamInfo: teamInfo,
+      })
+      .then(function (res) {
+        window.location = '/';
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.error(err.response.data.error);
+        setError(err.response.data.error);
+        // setTeamInfo('');
+      });
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">We are the champions</h1>
+      <form className="mt-5" onSubmit={onSubmitForm}>
+        <div className="d-flex">
+          <textarea
+            className="form-control"
+            placeholder="Enter team information"
+            rows="3"
+            required
+            value={teamInfo}
+            onChange={(e) => setTeamInfo(e.target.value)}
+          />
+          <button className="btn btn-success ml-2 mt-4 mb-4">Register</button>
+        </div>
+        <div>
+          {errorMessage && (
+            <div className="error">
+              <small className="text-danger"> {errorMessage} </small>
+            </div>
+          )}
+        </div>
+      </form>
+    </Fragment>
+  );
+};
+
+export default NewRegistration;
