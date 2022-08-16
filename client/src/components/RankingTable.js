6f49116ae9b248edec47e9514c6ce4c519706c2d
jonasngs <jonasngs30@gmail.com>
2022-08-16 22:31:06 +0800

    Add ranking table component

---
+++ client/src/components/RankingTable.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+
+const RankingTable = ({ groupNo, data }) => {
+  return (
+    <Fragment>
+      <h4 className="mt-5 text-center">Group {groupNo} team ranking</h4>
+      <table className="table  text-center">
+        <thead>
+          <tr>
+            <th>Team Name</th>
+            <th>Registration Date</th>
+            <th>Goals</th>
+            <th>Score</th>
+            <th>Alternate Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data[groupNo]?.map((rank) => (
+            <tr>
+              <td>{rank.teamName}</td>
+              <td>{rank.registrationDate}</td>
+              <td>{rank.goalsNo}</td>
+              <td>{rank.score}</td>
+              <td>{rank.altScore}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default RankingTable;
