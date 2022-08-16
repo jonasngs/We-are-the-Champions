a0d5724456125f80dec71dfa08fbae5b4f9b6322
jonasngs <jonasngs30@gmail.com>
2022-08-16 22:29:44 +0800

    Add result component

---
+++ client/src/components/AddResults.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+
+const AddResults = () => {
+  const [matchResults, setMatchResults] = useState('');
+    const [errorMessage, setError] = useState('');
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    axios
+      .put('http://localhost:5000/results', {
+        matchResults: matchResults,
+      })
+      .then(function (res) {
+        window.location = '/';
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.error(err.response.data.error);
+        setError(err.response.data.error);
+        // setMatchResults('');
+      });
+  };
+
+  return (
+    <Fragment>
+      <form className="mt-5" onSubmit={onSubmitForm}>
+        <div className="d-flex">
+          <textarea
+            className="form-control"
+            placeholder="Enter match results"
+            rows="3"
+            required
+            value={matchResults}
+            onChange={(e) => setMatchResults(e.target.value)}
+          />
+          <button className="btn btn-primary ml-2 mt-4 mb-4">Add</button>
+        </div>
+        <div>
+          {errorMessage && (
+            <div className="error">
+              <small className="text-danger"> {errorMessage} </small>
+            </div>
+          )}
+        </div>
+      </form>
+    </Fragment>
+  );
+};
+
+export default AddResults;
